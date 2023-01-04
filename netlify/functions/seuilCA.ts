e383f8a6beeaeaaf2dd3f0c9caaee0f466898cf9
samaty59 <you@example.com>
2023-01-04 01:23:43 +0100

    sa

--- netlify/functions/seuilCA.ts
+++ netlify/functions/seuilCA.ts
@@ -1,17 +1,17 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event) => {
-  // const { revenuFraisPro, quotientFamilial, rate, abattement }  = event.queryStringParameters
+  const { revenuFraisPro, quotientFamilial, rate, abattement }  = event.queryStringParameters
 
-  // let caSeuil = 0.11 * (revenuFraisPro - quotientFamilial*10777)/(rate - abattement*0.11);
-  // caSeuil = caSeuil < 0 ? 0 : caSeuil;
+  let caSeuil = 0.11 * (+revenuFraisPro - +quotientFamilial*10777)/(+rate - +abattement*0.11);
+  caSeuil = caSeuil < 0 ? 0 : caSeuil;
 
   console.log(event.queryStringParameters)
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      // message: `Seuil de Chiffre d'Affaire, ${caSeuil}`,
+      message: `Seuil de Chiffre d'Affaire, ${caSeuil}`,
     }),
   }
 }
