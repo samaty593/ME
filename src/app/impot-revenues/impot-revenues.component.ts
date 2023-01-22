fd25dfcba2b0b4adc8cad6df0a3d1cbd00391fa1
samaty59 <you@example.com>
2023-01-22 11:45:54 +0100

    table styling done

--- src/app/impot-revenues/impot-revenues.component.ts
+++ src/app/impot-revenues/impot-revenues.component.ts
@@ -1,41 +1,73 @@
 import { Component } from '@angular/core';
 import { ImpotRevenuService } from '../impot-revenu.service';
 
+interface StoredSimu {
+  quotientFamilial: number,
+  salaires: number,
+  chiffreAffaire: number,
+  irSansVfl: number,
+  irAvecVfl: number,
+}
+
 @Component({
   selector: 'impot-revenues',
   templateUrl: './impot-revenues.component.html',
   styleUrls: ['./impot-revenues.component.scss']
 })
 export class ImpotRevenuesComponent {
-  constructor(private impotRev: ImpotRevenuService) {}
+  count: number = 0;
+  constructor(private impotRev: ImpotRevenuService) {
+    const store = localStorage.getItem('cachedIrSimulation')
+    this.storedSimulation = store ? JSON.parse(store) : null;
+  }
 
-  maritalStatus: string;
-  activityType: string;
-  quotientFamilial: number;
-  salaires: number;
-  chiffreAffaire: number;
-  irSansVfl: string;
-  irAvecVfl: string;
+  maritalStatus?: string;
+  activityType?: string;
+  quotientFamilial?: number;
+  salaires?: number;
+  chiffreAffaire?: number;
+  irSansVfl: number;
+  irAvecVfl: number;
+  storedSimulation: StoredSimu[] = [];
 
   estimateImpRev() {
     let params = {
-      maritalStatus: this.maritalStatus,
       quotientFamilial: this.quotientFamilial,
       salaires: this.salaires,
       chiffreAffaire: this.chiffreAffaire,
+      maritalStatus: this.maritalStatus,
       activityType: this.activityType,
     }
-
+  
     this.impotRev.estimateImpotRev(params).subscribe(r => {
-      this.irSansVfl = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(r.IrSansVfl);
-      this.irAvecVfl = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(r.IrAvecVfl);
+      this.irSansVfl = r.IrSansVfl;
+      this.irAvecVfl = r.IrAvecVfl;
 
-      localStorage.setItem
+      let storedSimulation = {
+        quotientFamilial: this.quotientFamilial,
+        salaires: this.salaires,
+        chiffreAffaire: this.chiffreAffaire,
+        irSansVfl: this.irSansVfl,
+        irAvecVfl: this.irAvecVfl,
+      };
+
+      const store = JSON.parse(localStorage.getItem('cachedIrSimulation'));
+
+      if(!store) {
+        this.storedSimulation = [storedSimulation];
+        localStorage.setItem('cachedIrSimulation', `${JSON.stringify([storedSimulation])}`);
+      }
+      else {
+        this.storedSimulation = [...this.storedSimulation, storedSimulation];
+        console.log(this.storedSimulation)
+        localStorage.setItem('cachedIrSimulation', `${JSON.stringify(this.storedSimulation)}`)
+      }
     })
   }
 
   clearData() {
     localStorage.clear();
+    this.storedSimulation = [];
   }
 
 }
