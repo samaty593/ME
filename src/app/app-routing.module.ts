63f5185ec622644f40de9f97d3995b0119529b98
samaty59 <you@example.com>
2023-01-25 13:43:58 +0100

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { SeuilChiffreAffaireComponent } from './seuil-chiffre-affaire/seuil-chiffre-affaire.component';
 
 const routes: Routes = [
+  { path: "", component: ImpotRevenuesComponent },
   { path: "seuil-chiffre-d'affaire", component: SeuilChiffreAffaireComponent },
   { path: "impot-revenues-versement-forfaitaire", component: ImpotRevenuesComponent },
   { path: '**', component: PagenotfoundComponent },
