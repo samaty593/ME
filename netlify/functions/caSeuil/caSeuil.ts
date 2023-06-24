4664cde1d14b41d4008cd3d36508b1c8c1dde8f3
samaty59 <you@example.com>
2023-06-24 16:50:32 +0200

    hide and seek

--- netlify/functions/caSeuil/caSeuil.ts
+++ netlify/functions/caSeuil/caSeuil.ts
@@ -25,14 +25,14 @@ export const handler: Handler = async (event: HandlerEvent) => {
 
  try {
 
-  let caSeuil: number = 0.11 * (+revenuFraisPro - +quotientFamilial*10777)/(matrix.rate - matrix.abattement*0.11);
-  caSeuil = +caSeuil.toFixed(0);
-  caSeuil = caSeuil < 0 ? 0 : caSeuil;
+  // let caSeuil: number = 0.11 * (+revenuFraisPro - +quotientFamilial*10777)/(matrix.rate - matrix.abattement*0.11);
+  // caSeuil = +caSeuil.toFixed(0);
+  // caSeuil = caSeuil < 0 ? 0 : caSeuil;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      caseuil: caSeuil,
+      // caseuil: caSeuil,
     }),
   }
  } catch(err) {
